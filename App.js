ed1f8821219cc68ff6546dcec7efa9c0920be61d
Rafael Mateus Marques <rafrito@DP6-PE08DV12>
2023-05-02 01:29:43 -0300

    feat: first identation changes

--- App.js
+++ App.js
@@ -4,16 +4,18 @@ import { StyleSheet, View, Button, Text } from 'react-native';
 export default function StartScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Lobisomem Por Uma Noite</Text>
+      <Text style={styles.title}>Lobisomem{"\n"}por uma noite</Text>
       <View style={styles.buttonContainer}>
         <Button
           title="Jogar"
           onPress={() => navigation.navigate('RoleSelection')}
         />
+        <View style={styles.separator} />
         <Button
           title="Regras"
           onPress={() => navigation.navigate('Rules')}
         />
+        <View style={styles.separator} />
         <Button
           title="Personagens"
           onPress={() => navigation.navigate('Characters')}
@@ -30,12 +32,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
-    fontSize: 30,
+    fontSize: 36,
     fontWeight: 'bold',
-    marginBottom: 50,
+    marginBottom: 30,
+    textAlign: 'center',
+    paddingHorizontal: 30,
   },
   buttonContainer: {
     width: '80%',
-    marginBottom: 20,
+  },
+  separator: {
+    height: 10,
   },
 });
